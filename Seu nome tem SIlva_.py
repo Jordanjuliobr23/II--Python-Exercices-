58bea108121ec44d337a7dc62179df983796d5d1
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-04-03 18:30:54 -0300

    Add files via upload

---
+++ Seu nome tem SIlva_.py
@@ -0,0 +1,8 @@
+print('=-'*100)
+print('Vc tem SILVA no nome?')
+n=str(input('Digite seu nome completo:'))
+print('Seu nome tem Silva?')
+if 'Silva' in n: 
+    print('Silva'in n)
+else:
+    print('Vc não tem Silva no seu nome!')
