d13aa1e11f4f54a70c076a37f715c2fc9788819d
Jordan Júlio <164351071+Jordanjuliobr23@users.noreply.github.com>
2024-04-04 09:44:20 -0300

    Add files via upload

---
+++ Operações envolvendo dois números digitados pelo usuário.py
@@ -0,0 +1,11 @@
+v1=int(input('Digite um valor:'))
+v2=int(input('Digite outro valor:'))
+a= v1 + v2
+s= v1 - v2
+m= v1 * v2
+d= v1 / v2
+p= v1 ** v2
+di= v1 // v2
+rd= v1 % v2
+print('A soma vale {} \n a diferença vale {} \n o produto vale {} \n a divisão {} \n'.format(a,s,m,d))
+print('A pontencia vale {} \n a divisão inteira {} \n o resto da divisão {}'.format(p,di,rd))
